9836471b5bf7c959f82a39c77584628dc3fbe8ff
Jocelyn Q <jocelyn.develop@gmail.com>
2023-09-27 19:54:11 +0200

    [ADD] fix spawn ground

--- game/graphic.js
+++ game/graphic.js
@@ -43,12 +43,19 @@ function Ground(color, size_x, size_y, nb_tile) {
     for (y = minY; y <= maxY; y = y + sizeOfTileY) {
       color = colors[Math.floor(Math.random() * colors.length)];
 
-      if (0x000000 != color) {
+      if (x == 60 && y == 0) {
+        tmpGround = new THREE.Mesh(new THREE.PlaneGeometry(sizeOfTileX - 10, sizeOfTileY - 10), new THREE.MeshLambertMaterial({ color: 0x0000ff, transparent: true, opacity: 0.6 }));
+        tmpGround.position.x = x;
+        tmpGround.position.y = y;
+        scene.add(tmpGround);
+      } else if (0x000000 != color) {
         tmpGround = new THREE.Mesh(new THREE.PlaneGeometry(sizeOfTileX - 10, sizeOfTileY - 10), new THREE.MeshLambertMaterial({ color: color, transparent: true, opacity: 0.6 }));
         tmpGround.position.x = x;
         tmpGround.position.y = y;
         scene.add(tmpGround);
-      } else noGround.push([x, y]);
+      } else {
+        noGround.push([x, y]);
+      }
     }
   }
 }
