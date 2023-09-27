792156f95cfc140986979a282f11854b4ed53cac
Jocelyn Q <jocelyn.develop@gmail.com>
2023-09-27 19:58:07 +0200

    [ADD] other enemy

--- game/graphic.js
+++ game/graphic.js
@@ -24,6 +24,9 @@ function init() {
   player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
   scene.add(player1.graphic);
 
+  ennemy1 = new Player("ennemy1", 0x800080, new THREE.Vector2(30, 0), 0);
+  scene.add(ennemy1.graphic);
+
   light1 = new Light("sun", 0xffffff, "0,0,340");
   scene.add(light1);
 }
